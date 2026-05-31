58e3b03d5ef17be34d2163511ffd2e5799322040
yash-kumawat-0 <kumawatyash378@gmail.com>
2026-05-31 13:21:00 +0530

    fix: remove app.options wildcard - Express 5 incompatible

--- server/server.js
+++ server/server.js
@@ -29,7 +29,6 @@ const corsOptions = {
 const app = express();
 
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
